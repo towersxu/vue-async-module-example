13c337a86858ca012e7da6f7616c9c1b4000468b
Evan You <yyx990803@gmail.com>
2015-12-17 22:22:00 -0500

    adjust build setup

---
+++ build/webpack.base.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        // edit this for additional asset file types
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  // vue-loader config:
+  // lint all JavaScript inside *.vue files with ESLint
+  vue: {
+    loaders: {
+      js: 'babel!eslint'
+    }
+  },
+  // example: if you wish to apply custom babel options
+  // instead of using vue-loader's default:
+  babel: {
+    presets: ['es2015', 'stage-0'],
+    plugins: ['transform-runtime']
+  }
+}
