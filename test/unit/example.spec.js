9d2bcc6332f4699f43648c3fb460b8f74b8d9362
Evan You <yyx990803@gmail.com>
2015-12-21 18:07:49 -0500

    comments

--- test/unit/example.spec.js
+++ test/unit/example.spec.js
@@ -1,15 +1,24 @@
 /* global describe, it, expect */
 
 import Vue from 'vue'
+
+// inject-loader gives us a factory that can create instances
+// of the module with different injected dependencies.
+// make sure to use the require() syntax here.
+// for webpack loader string syntax, see:
+// - https://webpack.github.io/docs/loaders.html
 const ExampleInjector = require('!!vue?inject!./example.vue')
 
 describe('component with dependency injection example', () => {
   it('should be injectable', () => {
+    // create an instance of the Example module,
+    // injecting a mocked "../service" dependency
     const Example = ExampleInjector({
       '../service': {
         msg: 'Hello from mock'
       }
     })
+    // now we can test it! 
     const vm = new Vue({
       template: '<div><test></test></div>',
       components: {
--- test/unit/example.vue
+++ test/unit/example.vue
@@ -3,6 +3,9 @@
 </template>
 
 <script>
+// this is an example component with external dependencies.
+// the "../service" module doesn't actually exists, but we
+// will inject it in our tests!
 import SomeService from '../service'
 
 export default {
